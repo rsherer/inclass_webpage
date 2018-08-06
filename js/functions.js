da4321606e8e8925f0841ad470d4ad408ee0af8c
Miles Erickson <miles.erickson@gmail.com>
2018-08-06 16:38:46 -0700

    Move function to js/functions.js

---
+++ js/functions.js
@@ -0,0 +1,6 @@
+function changeImage() {
+    imgRM = document.getElementById('RM')
+    imgRM.style.display = "block"
+    imgRF = document.getElementById('RF')
+    imgRF.style.display = "none"
+}
